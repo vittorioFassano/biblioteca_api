043d1ccab4af9167cb52a5fd7da4623711c09dfe
vittorio <vittfassano@gmail.com>
2024-05-14 17:48:45 -0300

    create project

---
+++ src/main/java/org/serratec/backend/bibliotecavittorio/BibliotecavittorioApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.bibliotecavittorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecavittorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecavittorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/backend/bibliotecavittorio/BibliotecavittorioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.bibliotecavittorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecavittorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
