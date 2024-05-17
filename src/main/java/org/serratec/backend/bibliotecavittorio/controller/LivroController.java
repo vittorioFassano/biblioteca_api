68850de9ca1092deb84e4e4d71ca25990b2828c2
vittorio <vittfassano@gmail.com>
2024-05-17 11:19:20 -0300

    feat put livro

--- src/main/java/org/serratec/backend/bibliotecavittorio/controller/LivroController.java
+++ src/main/java/org/serratec/backend/bibliotecavittorio/controller/LivroController.java
@@ -2,15 +2,11 @@ package org.serratec.backend.bibliotecavittorio.controller;
 
 import java.util.List;
 
-import java.util.Optional;
-
 import org.serratec.backend.bibliotecavittorio.model.Livro;
 import org.serratec.backend.bibliotecavittorio.repository.LivroRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,25 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 
-
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
-	
+
 	@Autowired
 	private LivroRepository livroRepository;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Livro>> listar() {
 		return ResponseEntity.ok(livroRepository.findAll());
 	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Livro> cadastrar(@Valid @RequestBody Livro livro) {
+		Livro novoLivro = livroRepository.save(livro);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoLivro);
+	}
 	
-	 @PostMapping
-	    @ResponseStatus(HttpStatus.CREATED)
-	    public ResponseEntity<Livro> cadastrar(@Valid @RequestBody Livro livro) {
-	        Livro novoLivro = livroRepository.save(livro);
-	        return ResponseEntity.status(HttpStatus.CREATED).body(novoLivro);
-	    }
-	
+	@PutMapping("/{id}")
+	public ResponseEntity<Livro> alterar(@PathVariable Long id, @Valid @RequestBody Livro livro) {
+		if (!livroRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		livro.setId(id);
+		livro = livroRepository.save(livro);
+		return ResponseEntity.ok(livro);
+	}
 
 }
